49d2c48bc7e50e2522cd671c25db3372645bfcc9
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2021-04-20 23:47:53 -0500

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,3 @@
+
+rootProject.name = "KotlinersKtSchool"
+
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,41 @@
+
+const val chuby = "Donde estan mis poptarts?!!!"
+const val sier = 123234
+const val biteS:Byte = 0b101
+var gorro = "NOOOO!!! 🤡"
+val chubyDosQueEsSier = "Que no me gustan las sandias ya les dije!"
+
+fun main() {
+
+    val kotlinersNoSe: String = "Sier, Chuby y Gorro"
+    val age:
+    val letter: Char = 'G'
+    val decition: Boolean = true
+    val _chubyAge: Float = 60.5f
+    val ChubyAge: Float = 60.5f
+    val chubyAge: Float = 60.5f
+    val gorroAge: Int = 12
+
+    var otraVariable:Double = 123.0
+    otraVariable = 5/2.0
+
+    var otraVariableFlotante = 123.123F
+    otraVariable = 5.8
+
+    val numberForever: Double = 5.6
+
+    var litros: Int = 34
+    println(litros)
+
+//    numberForever = 8.6
+
+    litros = 30
+    println(litros)
+    litros = 50
+    println(litros)
+    litros = 10
+
+    println(numberForever)
+    println(litros)
+
+}
